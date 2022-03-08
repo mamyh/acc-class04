5d929052ff853d4ab7cd55c3567b3903a85bd8a5
mamyh <likhmaon@gmail.com>
2022-03-08 16:50:41 +0600

    initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import TheLayout from "./views/pages/TheLayout";
 
 function App() {
-  const [first, setfirst] = useState({});
   return (
     <div>
       <TheLayout />
